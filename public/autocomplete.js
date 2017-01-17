9e071ef13d614565e740f9876eb683631553c947
Andie Romero <andriana@beltranfamily.com>
2017-01-17 12:28:03 -0800

    format list, wrap each item in a link to show

--- public/autocomplete.js
+++ public/autocomplete.js
@@ -2,13 +2,16 @@
 var $input = $('#search');
 var $list = $('#list');
 
-
-$input.keyup(function(e){
+function clearList(items){
 	// clear list before displaying new list
-	// while ($list.hasChildNodes()) {
- //    	$list.removeChild($list.lastChild);
-	// }
-
+	var i = 0;
+	while(i < items.length){
+		items[i].remove();
+		i++;
+	}
+}
+$input.keyup(function(e){
+	clearList($('.item'));
 	var searchStr = $input.val();
 	var date = new Date();
 	date = Math.floor(date.getTime()/1000);
@@ -16,9 +19,11 @@ $input.keyup(function(e){
 	$.get("https://api.viki.io/v4/search.json?c=" + searchStr + "&per_page=5&with_people=true&app=100266a&t=" + date,
 		function(data){
 			// console.log(data);
+			if($('.item').length != 0) {
+				clearList($('.item'));
+			}
 			$.each(data, function( i, obj) {
-				console.log(obj);
-				$list.append("<div>" + obj.tt + "</div>");
+				$list.append("<a target='_blank' href='https://www.viki.com" + obj.u.w + "'><li class='item'><img alt='movie' src='" + obj.i + "'><p>" + obj.tt + "</p></li></a>");
 				return ( i !== 4 );
 			});
 		});
