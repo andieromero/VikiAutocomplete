fc0be3902f5234710a3b4b6ced8f3f731967002e
Andie Romero <andriana@beltranfamily.com>
2017-01-17 18:02:36 -0800

    highlight items on up and down arrows

--- public/autocomplete.js
+++ public/autocomplete.js
@@ -9,46 +9,53 @@ function clearList($items){
 		$items[i].remove();
 		i++;
 	}
+	index = -1;
 }
 var autocomplete = function(e){
 	var $items = $('.item');
-	clearList($items);
+	// unhighlight current item
+	var prevItem = $items[index];
+	$(prevItem).removeClass('hover');
 
-	var searchStr = $input.val();
-	var date = new Date();
-	date = Math.floor(date.getTime()/1000);
-	// api call
-	$.get("https://api.viki.io/v4/search.json?c=" + searchStr + "&per_page=5&with_people=true&app=100266a&t=" + date,
-		function(data){
-			if($items.length != 0) {
-				clearList($items);
-			}
-			$.each(data, function( i, obj) {
-				$list.append("<a target='_blank' class='item' href='https://www.viki.com" + obj.u.w + "'><li><img alt='movie' src='" + obj.i + "'><p>" + obj.tt + "</p></li></a>");
-				return ( i !== 4 );
-			});
-		});
-	if($items.length != 0){
-		var prevItem = $items[index];
-		$(prevItem).removeClass('hover');
-		// down arrow
-		if(e.keyCode == 40){
-			console.log("down");
-			index++;
-		}
-		// up arrow
-		else if (e.keyCode == 38){
-			console.log("up");
-			index--;
-		}
-		// enter
-		else if (e.keyCode == 13){
-			console.log("enter");
-		}
-		else {}
-		var currItem = $items[index];
-		$(currItem).addClass('hover');
-	}	
-}
-// populate list while typing
+	var keyCode = e.keyCode || e.which;
+	// down arrow
+	if(keyCode === 40){
+		console.log("down");
+		index++;
+	}
+	// up arrow
+	else if (keyCode === 38){
+		console.log("up");
+		index--;
+	}
+	// enter
+	// else if (keyCode === 13){
+	// 	e.preventDefault();
+	// 	console.log("enter");
+	// }
+	// typing
+	else {
+		// reset list and index
+		clearList($items);
+
+		var searchStr = $input.val();
+		var date = new Date();
+		date = Math.floor(date.getTime()/1000);
+		// api call
+		$.get("https://api.viki.io/v4/search.json?c=" + searchStr + "&per_page=5&with_people=true&app=100266a&t=" + date,
+			function(data){
+				if($items.length != 0) {
+					clearList($items);
+				}
+				$.each(data, function( i, obj) {
+					$list.append("<a target='_blank' class='item' href='https://www.viki.com" + obj.u.w + "'><li><img alt='movie' src='" + obj.i + "'><p>" + obj.tt + "</p></li></a>");
+					return ( i !== 4 );
+				});
+			});	
+	}
+	var currItem = $items[index];
+	$(currItem).addClass('hover');
+};
+
+// populates list, highlights items
 $input.keyup(autocomplete);
